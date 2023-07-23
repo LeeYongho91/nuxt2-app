f225d755e0722e939a6fe5d8982af28b461333d2
LeeYongho91 <skyks4848@naver.com>
2023-07-23 20:59:57 +0900

    20230723

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      <NuxtLink to="/">Nuxt Shopping</NuxtLink>
+   </h1>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
