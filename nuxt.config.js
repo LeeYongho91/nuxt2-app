e82c21d806d76d76ffeba183f2f63e13af0e4b15
LeeYongho91 <skyks4848@naver.com>
2023-07-25 23:51:16 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
+  enerate: {
+    fallback: true
+  },
   head: {
     title: 'nuxt-app',
     htmlAttrs: {
